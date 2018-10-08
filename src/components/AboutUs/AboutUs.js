40b052db5f93796758e83358ccdd49166210ab6b
amicokaur <amica0811@yaho..com>
2018-10-08 15:53:56 -0700

    styling

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -2,10 +2,14 @@ import {Row, Col} from "react-bootstrap"
 import "./AboutUs.css"
 import React from "react"
 
+const backgroundImg = {
+  backgroundImage: "url('/backgroudmain2.jpg')"
+}
+
 const AboutUs = () => (
     <Row >
     
-    <Col md={12} xs={10} id="aboutUsBackground" >
+    <Col md={12} xs={10} id="aboutUsBackground" style={backgroundImg}>
       <a href="/aboutUs" id="aboutUsLettering">About us</a>
     </Col>
   </Row>
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,14 +1,18 @@
 import React from "react"
 import {Grid, Col, Row} from "react-bootstrap"
 import logo from  "./../../images/GarageGoods.gif"
+// import backgroundPic from "/backgroundmain2.jpg"
 import "./Wrapper.css"
+
+
 const Wrapper = (props) => (
     <Grid fluid id= "wrapper">
      <Row>
-        <Col md={4} style={{background:"black", height:900}}>
-            <img alt="none" style={{width:"100%",paddingTop:"3rem"}} src={logo} ></img>
+        <Col md={4} >
+            <img alt="none" style={{width:"100%", paddingTop:"3rem"}} src={logo} ></img>
         </Col>
         <Col md={8} style={{background:"lightGray", height:900, paddingTop:"8rem"}}>
+        {/* <img src={this.props.url} alt=""  className="img-responsive" /> */}
             {props.children}
         </Col>
      </Row>
