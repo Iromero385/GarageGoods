db1b24d01998130a3161e461b2ad5de98e7397bd
amicokaur <amica0811@yaho..com>
2018-10-11 19:59:17 -0700

    added coloring to collapse toogle

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -8,12 +8,12 @@ const backgroundImg = {
 
 class AboutUs extends React.Component {
   state = {
-    open:false
+    open: false
   }
   handleCollapseToogle = () => {
-    this.setState({open:!this.state.open})
+    this.setState({ open: !this.state.open })
   }
-  render(){
+  render() {
     return (
 
       <Row >
@@ -21,22 +21,19 @@ class AboutUs extends React.Component {
           <br />
           <div className="aboutUsLettering" onClick={() => this.handleCollapseToogle()} >About us</div>
           <Collapse in={this.state.open} >
-          <div>
-            <Well style={{backgroundColor:"transparent",color:"white",fontSize:"3rem"}}>
-            About GarageGoods
-We collect, review and deliver the best information --- so you don’t have too.
-
-GarageGoods was co-founded in 2018 by Isaac Romero and Amico Kaur (students at UC Extentions as Full stack Developers) instructed by David Hallinan (). 
-What We Do 
-
-At GarageGoods , we collect, review, and deliver the best information in the market, so you don’t have to. Our data collects the presents and future garage sales and can be search by categories, city and zip code. You can also create, check your post and review your incoming garage sales.  
-
-
+            <div>
+              <Well style={{ backgroundColor: "transparent", color: "white", fontSize: "3rem" }}>
+                <p>About GarageGoods</p>
+                <p>We collect, review and deliver the best information --- so you don’t have too.
+                GarageGoods was co-founded in 2018 by Isaac Romero and Amico Kaur (students at UC Extentions as Full stack Developers) instructed by David Hallinan ().
+                What We Do At GarageGoods , we collect, review, and deliver the best information in the market, so you don’t have to. Our data collects the presents and future garage sales and can be search by categories, city and zip code. You can also create, check your post and review your incoming garage sales.
+                </p>
+    
             </Well>
-          </div>
-        </Collapse>
+            </div>
+          </Collapse>
         </Col>
-        <div style={{backgroundColor:"lightgray", width:"100%",height:100, marginLeft:0,marginRight:0, marginTop:"50rem" }}></div>
+        <div style={{ backgroundColor: "lightgray", width: "100%", height: 100, marginLeft: 0, marginRight: 0, marginTop: "50rem" }}></div>
       </Row>
     );
   }
